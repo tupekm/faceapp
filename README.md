ea6ad0601161b06367c8059420edd1499bdac7aa
tupekm <marintupek@gmail.com>
2019-11-11 14:54:23 +0000

    Frontend created - styles, positions

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Web application for face detection
 
 1. Cloned repository to local machine
 2. Created react application:
-- # npm install -g create-react-app
-- # create-react-app faceapp
+-  npm install -g create-react-app
+-  create-react-app faceapp
--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+import Particles from 'react-particles-js';
 
+const particlesOptions = {
+  
+    particles: {
+      number: {
+        value:150,
+        density: {
+          enable: true,
+          value_area: 800,
+          shadow: {
+            enable: true,
+            color: "#3CA9D1",
+            blur: 5
+          }
+        }
+      }
+    }
+  }
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Particles className='particles' params={particlesOptions}/>
+    <Navigation />
+    <Logo />
+    <Rank />
+    <ImageLinkForm />
+    {/**
+    <FaceRecognition />*/}
+      
     </div>
   );
 }
